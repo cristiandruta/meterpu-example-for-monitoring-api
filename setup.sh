7a04266ad9e044071d57ff19f5189482e44b26fb
lilu09 <lu.li@liu.se>
2016-08-04 11:22:10 +0200

    minor fix

--- setup.sh
+++ setup.sh
@@ -16,18 +16,31 @@
 
 
 #set up monitoring api
-git clone https://github.com/excess-project/monitoring-api.git
+set_up_monitoring_api()
+{
+	git clone https://github.com/excess-project/monitoring-api.git
+	cd monitoring-api
+	./setup.sh
+	cd -
+}
 
 #set up Nvidia GDK
-GDK_PATH="./nvidia_gdk"
-mkdir -p $GDK_PATH
-cd $GDK_PATH
-NVIDIA_BASE_URL="http://developer.download.nvidia.com"
-NVIDIA_GDK="gdk_linux_amd64_352_55_release.run"
-wget ${NVIDIA_BASE_URL}/compute/cuda/7.5/Prod/gdk/${NVIDIA_GDK}
-if [ ! -f ${NVIDIA_GDK} ]; then
-    echo "[ERROR] File not found: " ${NVIDIA_GDK}
-    exit 1;
-fi
-chmod +x ${NVIDIA_GDK}
-./${NVIDIA_GDK} --silent --installdir=`realpath .`
+set_up_nvidia_gdk()
+{
+	GDK_PATH="./nvidia_gdk"
+	mkdir -p $GDK_PATH
+	cd $GDK_PATH
+	NVIDIA_BASE_URL="http://developer.download.nvidia.com"
+	NVIDIA_GDK="gdk_linux_amd64_352_55_release.run"
+	wget ${NVIDIA_BASE_URL}/compute/cuda/7.5/Prod/gdk/${NVIDIA_GDK}
+	if [ ! -f ${NVIDIA_GDK} ]; then
+		echo "[ERROR] File not found: " ${NVIDIA_GDK}
+		exit 1;
+	fi
+	chmod +x ${NVIDIA_GDK}
+	./${NVIDIA_GDK} --silent --installdir=`realpath .`
+}
+
+
+set_up_monitoring_api
+set_up_nvidia_gdk
